9afe8ac0b8071d65979f6fcc54194dc9e8f36da2
Monse180305 <2233073091@correo.ler.uam.mx>
2025-10-21 19:23:19 -0600

    clases

---
+++ ejemplo.p5/sketch.js
@@ -0,0 +1,10 @@
+function setup(){
+    createCanvas(500, 500);
+}
+
+function draw(){
+    background (90, 100, 250);
+    rect (28,62,25,30);
+    circle (40,50,20,30,10,15,30);
+    fill (0,0,0);
+}
